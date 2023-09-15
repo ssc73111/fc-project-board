aac30df137277ab00879c8d683baf35000818a0f
my <pd73111@gmail.com>
2023-09-15 17:00:15 +0900

    #12 - 데이터베이스 접근 로직 3
    데이터베이스 접근 로직 3

--- src/main/java/com/fc/projectboard/domain/Article.java
+++ src/main/java/com/fc/projectboard/domain/Article.java
@@ -1,22 +1,78 @@
 package com.fc.projectboard.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createBy")
+}) // 위의 인덱스로 빠른 서칭 가능하도록 함.
+@Entity
 public class Article {
-    private Long id;
-    private String title; // 제목
-    private String content; // 본문
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 자동으로 부여하는 값으로 사용자가 수정 하지 못하도록 한다. (JPA Persistence Context 가 영속화를 할때 자동으로 부여해주는 고유번호)
 
+    @Setter
+    @Column(nullable = false)
+    private String title; // 제목
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content; // 본문
+    // 옵셔널
+    @Setter
+    private String hashtag; // 해시태그
+
+    // 메타데이터는 자동 세팅되므로 Setter 를 사용하지 않음.
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createBy; // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자
 
+    protected Article() {
+    }
 
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id != null && Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
