3b3f9bd334211e0d89419e7eb3ea989bd12ce4bf
my <pd73111@gmail.com>
2023-09-20 15:31:08 +0900

    #6 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    - 'QuerydslPredicateExecutor', 'QuerydslBinderCustomizer' 를 이용하여 간단하게 개발

--- src/main/java/com/fc/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fc/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,34 @@
 package com.fc.projectboard.repository;
 
 import com.fc.projectboard.domain.Article;
+import com.fc.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // Entity 안에 있는 모든 검색 기능 추가
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // 선택적 검색
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createBy);
+//        bindings.bind(root.title).first(SimpleExpression::eq); // Exactly(완전일치) 검색을 바꿔준다.
+//    bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%' contain 기능으로 쿼리가 다름.
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+    }
+
+    ;
 }
+
