920b2ba0e649e4d45fbc11f933ef7987acf2ca9b
my <pd73111@gmail.com>
2023-11-09 09:28:13 +0900

    #24 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 `Page` 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/com/fc/projectboard/controller/ArticleController.java
+++ src/main/java/com/fc/projectboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.fc.projectboard.domain.type.SearchType;
 import com.fc.projectboard.dto.response.ArticleResponse;
 import com.fc.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.fc.projectboard.service.ArticleService;
+import com.fc.projectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * /articles
@@ -27,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -34,18 +39,22 @@ public class ArticleController {
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map) {
-        map.addAttribute(
-                "articles",
-                articleService.searchArticles(searchType, searchValue, pageable)
-                        .map(ArticleResponse::from)
-        );
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable)
+                .map(ArticleResponse::from);
+        map.addAttribute("articles", articles);
+
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
-        map.addAttribute("article", article                );
+        map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
 
         return "articles/detail";
--- src/test/java/com/fc/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fc/projectboard/controller/ArticleControllerTest.java
@@ -4,6 +4,7 @@ import com.fc.projectboard.config.SecurityConfig;
 import com.fc.projectboard.dto.ArticleWithCommentsDto;
 import com.fc.projectboard.dto.UserAccountDto;
 import com.fc.projectboard.service.ArticleService;
+import com.fc.projectboard.service.PaginationService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,15 +13,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -33,7 +36,10 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
-    @MockBean private ArticleService articleService;
+    @MockBean
+    private ArticleService articleService;
+    @MockBean
+    private PaginationService paginationService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -46,14 +52,46 @@ class ArticleControllerTest {
 
         given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class)))
                 .willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
 
         mvc.perform(get("/articles"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-            .andExpect(view().name("articles/index"))
-            .andExpect(model().attributeExists("articles")); // 해당 이름의 데이터가 있는지 확인
-
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles")) // 해당 이름의 데이터가 있는지 확인
+                .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 페이징, 정렬 기능")
+    @Test
+    void givenPagingAndSortingParams_whenSearchingArticlesPage_thenReturnsArticlesPage() throws Exception {
+        // Given
+        String sortName = "title";
+        String direction = "desc";
+        int pageNumber = 0;
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc(sortName)));
+        List<Integer> barNumbers = List.of(1, 2, 3, 4, 5);
+        given(articleService.searchArticles(null, null, pageable)).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages())).willReturn(barNumbers);
+
+        // When & Then
+        mvc.perform(
+                        get("/articles")
+                                .queryParam("page", String.valueOf(pageNumber))
+                                .queryParam("size", String.valueOf(pageSize))
+                                .queryParam("sort", sortName + "," + direction)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attribute("paginationBarNumbers", barNumbers));
+        then(articleService).should().searchArticles(null, null, pageable);
+        then(paginationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
     }
 
 
@@ -64,11 +102,11 @@ class ArticleControllerTest {
         given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         mvc.perform(get("/articles/" + articleId))
-            .andExpect(status().isOk())
-            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-            .andExpect(view().name("articles/detail"))
-            .andExpect(model().attributeExists("article")) // 해당 이름의 데이터가 있는지 확인
-            .andExpect(model().attributeExists("articleComments")); // 해당 이름의 데이터가 있는지 확인
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article")) // 해당 이름의 데이터가 있는지 확인
+                .andExpect(model().attributeExists("articleComments")); // 해당 이름의 데이터가 있는지 확인
 
         then(articleService).should().getArticle(articleId);
     }
@@ -78,9 +116,9 @@ class ArticleControllerTest {
     @Test
     void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
         mvc.perform(get("/articles/search"))
-            .andExpect(status().isOk())
-            .andExpect(view().name("articles/search "))
-            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search "))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현 중")
@@ -88,9 +126,9 @@ class ArticleControllerTest {
     @Test
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleSearchView() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
-            .andExpect(status().isOk())
-            .andExpect(view().name("articles/articles-hashtag"))
-            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/articles-hashtag"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     private ArticleWithCommentsDto createArticleWithCommentsDto() {
