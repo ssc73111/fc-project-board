d43217633c0d85937a8694b1371e56bf7831fa37
my <pd73111@gmail.com>
2023-11-24 10:59:50 +0900

    #52 - 도메인 업데이트
    이 pr은 1차 대댓글 개발을 위해 도메인을 업데이트한다.

--- src/main/java/com/fc/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fc/projectboard/domain/ArticleComment.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -25,25 +27,43 @@ public class ArticleComment extends AuditingFields {
     @Setter
     @ManyToOne(optional = false)
     private Article article; // 게시글 (ID)
+
     @Setter
     @ManyToOne(optional = false)
     @JoinColumn(name = "userId")
     private UserAccount userAccount; // 유저 정보 (ID)
+
     @Setter
     @Column(nullable = false, length = 500)
     private String content; // 본문
 
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글ID
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade= CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>(); // JPA 에서는 final 선언을 하지 않도록 권고
+
+
     protected ArticleComment() {
     }
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
