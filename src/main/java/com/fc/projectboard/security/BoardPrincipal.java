cea2b7a56384d2f138c0fcf5dc63be58db380f40
my <pd73111@gmail.com>
2023-11-29 09:47:54 +0900

    #62 - BoardPrincipal: OAuth 인증 정보를 다룰 수 있도록 기능 추가
    `OAuth2User` 인터페이스를 구현해서 Spring OAuth Client 도 기존과 동일하게 인증 정보를 인식할 수 있도록 함.

--- src/main/java/com/fc/projectboard/security/BoardPrincipal.java
+++ src/main/java/com/fc/projectboard/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,14 +18,24 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username,
                                     String password,
                                     String email,
                                     String nickname,
                                     String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username,
+                                    String password,
+                                    String email,
+                                    String nickname,
+                                    String memo,
+                                    Map<String, Object> oAuth2Attributes) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -35,7 +47,8 @@ public record BoardPrincipal(
                         .collect(Collectors.toUnmodifiableSet()),
                 email,
                 nickname,
-                memo);
+                memo,
+                oAuth2Attributes);
     }
 
     public static BoardPrincipal from(UserAccountDto dto) {
@@ -60,10 +73,15 @@ public record BoardPrincipal(
         return username;
     }
 
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attributes;
+    }
+
     // 유저 권한
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     // Spring Security 에 의존
@@ -86,6 +104,11 @@ public record BoardPrincipal(
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return username;
     }
 
     public enum RoleType {
