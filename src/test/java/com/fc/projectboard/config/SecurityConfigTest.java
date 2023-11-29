641d8d62ee9aeada00d4f5451040754e7d72eb96
my <pd73111@gmail.com>
2023-11-29 10:58:24 +0900

    #62 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다 OAuth 인증까지 받을 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/fc/projectboard/config/SecurityConfigTest.java
+++ src/test/java/com/fc/projectboard/config/SecurityConfigTest.java
@@ -1,7 +1,8 @@
 package com.fc.projectboard.config;
 
-import com.fc.projectboard.domain.UserAccount;
-import com.fc.projectboard.repository.UserAccountRepository;
+
+import com.fc.projectboard.dto.UserAccountDto;
+import com.fc.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -15,18 +16,33 @@ import static org.mockito.BDDMockito.given;
 public class SecurityConfigTest {
 
     @MockBean
-    private UserAccountRepository userAccountRepository;
+    private UserAccountService userAccountService;
+//    private UserAccountRepository userAccountRepository;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        given(userAccountRepository.findById(anyString()))
-                .willReturn(Optional.of(
-                        UserAccount.of(
-                                "unoTest",
-                                "password",
-                                "uno-test@mail.com",
-                                "uno-test",
-                                "memo test"
-                        )));
+//        given(userAccountRepository.findById(anyString()))
+//                .willReturn(Optional.of(
+//                        UserAccount.of(
+//                                "unoTest",
+//                                "password",
+//                                "uno-test@mail.com",
+//                                "uno-test",
+//                                "memo test"
+//                        )));
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                "unoTest",
+                "password",
+                "uno-test@mail.com",
+                "uno-test",
+                "memo test"
+        );
     }
 }
